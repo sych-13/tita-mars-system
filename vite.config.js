1b0b5e3bd4d0e4d3960f9d891546d12648707bda
sych-13 <jmtiosayco9963@gmail.com>
2026-09-21 21:40:30 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: '0.0.0.0' },
+
+  base: '/tita-mars-system/',
+
+  server: {
+    host: '0.0.0.0',
+  },
 });
